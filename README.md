60f8bfd65b7d2e5d7d2cfde8013cba7ca5f6c8f2
LukeEmery <luke.emery@lje.io>
2022-11-10 11:14:05 -0600

    checkpoint

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # .NET-Development-AWS-Workshop
+
+https://catalog.us-east-1.prod.workshops.aws/workshops/24a39743-7e40-48ca-ba5e-704f2e3d74ec/en-US
---
+++ SampleServer/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+
+using Nancy;
+using Nancy.Hosting.Self;
+using RandomNameGeneratorLibrary;
+
+namespace SampleServer
+{
+
+  public class VersionModule : NancyModule {
+    public VersionModule() {
+      Get("/", parameters => "Version 0.1");
+    }
+  }
+
+  class Program {
+    static PersonNameGenerator personGenerator = new PersonNameGenerator();
+    static Random random = new Random();
+    static void Main(string[] args) {
+      using (var nancyHost = new NancyHost(new Uri("http://localhost:8888/"))) {
+        nancyHost.Start();
+
+        Console.WriteLine("Nancy now listening - navigating to http://localhost:8888/. Press enter to stop");
+
+        var timer = new Timer(SavePolicy, null, 0, 3000);
+        Console.ReadKey();
+        timer.Dispose();
+      }
+
+      Console.WriteLine("Stopped. Good bye!");
+    }
+
+    private static void SavePolicy(object state) {
+      var policy = new Policy {
+        PolicyOwner = personGenerator.GenerateRandomFirstAndLastName(),
+        CprNo = GenerateCprNo()
+      };
+      PersistPolicy(policy);
+    }
+
+    private static string GenerateCprNo() {
+      var daysOld = random.Next(20 * 365, 100 * 365);
+      var bday = DateTime.Today.AddDays(-daysOld);
+      var seq = random.Next(1000, 9999);
+      return bday.ToString("ddMMyy") + "-" + seq.ToString();
+    }
+
+    private static void PersistPolicy(Policy policy) {
+      Console.WriteLine($"CPR: {policy.CprNo} Owner: {policy.PolicyOwner}");
+    }
+  }
+
+  public class Policy {
+    public string PolicyOwner { get; set; }
+    public string CprNo { get; set; }
+  }
+}
